30de99a7f9550d474453119801547ba842f14cab
glealdealmeida <62467337+glealdealmeida@users.noreply.github.com>
2020-03-24 14:00:49 +0100

    Update programme.py
    ajout du pari pair et correctif bug

--- programme.py
+++ programme.py
@@ -5,7 +5,7 @@ Created on Mon Mar 23 11:44:26 2020
 
 @author: guillaume
 """
-from random import shuffle, randint
+from random import shuffle
 from time import sleep
 regles =[]
 regles.append( "Le but de ce jeu est d'obtenir 21 points, ou de s'en rapprocher le plus possible, et ainsi battre le croupier")
@@ -20,9 +20,6 @@ couleur = ('Pique', 'Trefle', 'Carreau', 'Coeur')
 valeur = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10')
 
 paquet = []
-paquet_valeur = []
-main_joueur = []
-main_croupier = []
 solde = 500
 
 
@@ -40,17 +37,20 @@ def double(pair,solde):
         print("vous n'avez pas choisi de miser pair, le croupier va commencer")
         return pair
     else: 
-        while pair > solde or pair < 10:
+        while pair > solde  or solde < 10 and pair>0:
             if pair > solde :
                 pair = int(input("le croupier n'accepete pas votre mise, car vous n'avez pas assez"))
-            else :
+            elif pair < 10 and pair != 0:
                 pair = int(input("le croupier n'accepete pas votre mise, car la mise est inferieur a 10"))
+                
         return pair
 
 
 
 
 def initialisation(paquet):
+    print("le croupier prend un jeu de carte")
+    sleep(2)
     for col in range(4):
         for val in range(13):
             nouvelle_carte = (valeur[val], couleur[col])
@@ -59,6 +59,8 @@ def initialisation(paquet):
 
 
 def trier(paquet):
+    print("le croupier melange le jeu de carte")
+    sleep(2)
     shuffle(paquet)
     return paquet
 
@@ -68,28 +70,49 @@ def joueur(paquet):
     val_joueur = 0
     c = 0
     paquet_joueur = paquet
+    main_joueur = []
+    main_joueur2 = []
     jouer = True
     main_joueur.append(paquet_joueur[c])
     m = main_joueur[c]
     int_m = int(m[0])
     val_joueur = val_joueur + int_m
+    pari = int_m
+    del(paquet[c])
     c = 1
+    main_joueur2.append(paquet_joueur[c])
+    m = main_joueur2[0]
+    int_m = int(m[0])
+    pari2=int_m
+    c = 1
+    
+    sleep(2)
     while jouer is True and val_joueur<=21:
         main_joueur.append(paquet_joueur[c])
         m = main_joueur[c]
         int_m = int(m[0])
         val_joueur = val_joueur + int_m
         print(main_joueur,val_joueur)
-        c = c+1
         del(paquet_joueur[c])
+        del(paquet[c])
+        c = c+1
+        sleep(2)
         if val_joueur >21 :
             tirage = False
             print("c'est perdu !")
-            return val_joueur
+            del(c)
+            del(main_joueur)
+            del(paquet_joueur)
+            del(main_joueur2)
+            return val_joueur,pari,pari2,paquet
         elif val_joueur >21 :
             tirage = False
             print("Vous avez 21")
-            return val_joueur
+            del(c)
+            del(main_joueur)
+            del(paquet_joueur)
+            del(main_joueur2)
+            return val_joueur,pari,pari2,paquet
         else:
             tirage = int(input("tirer ?, si oui taper 1, sinon 0"))
             if tirage == 1:
@@ -100,7 +123,11 @@ def joueur(paquet):
                 jouer = False
                 print("vous avez donc :",val_joueur," le croupier pour vous battre doit au moins vous egaler")
                 sleep(2)
-                return val_joueur
+                del(c)
+                del(main_joueur)
+                del(main_joueur2)
+                del(paquet_joueur)
+                return val_joueur,pari,pari2,paquet
 
 
 
@@ -110,53 +137,85 @@ def croupier(paquet,val_joueur):
         jouer = False
         victoire = "lui"
         val_croupier = 0
-        return victoire,val_croupier
+        return victoire,val_croupier,paquet
     else :
-        t = 0
+        c = 0
         val_croupier = 0
+        main_croupier=[]
         paquet_croupier = paquet
-        i = randint(0,len(paquet_croupier))
-        main_croupier.append(paquet_croupier[i])
-        m = main_croupier[t]
+        main_croupier.append(paquet_croupier[c])
+        m = main_croupier[c]
+        del(paquet[c])
         int_m = int(m[0])
         val_croupier = val_croupier + int_m
-        t = 1
+        c = 1
+        sleep(2)
         while  jouer is True:
             print("le croupier joue")
-            i = randint(0,len(paquet_croupier))
-            main_croupier.append(paquet_croupier[i])
-            m = main_croupier[t]
+            main_croupier.append(paquet_croupier[c])
+            m = main_croupier[c]
             int_m = int(m[0])
             val_croupier = val_croupier + int_m
             print("le croupier a tire :",main_croupier,"il a donc un total de :",val_croupier)
-            t = t+1
-            del(paquet_croupier[i])
+            del(paquet_croupier[c])
+            del(paquet[c])
+            c = c+1
             sleep(2)
             if val_croupier > 21 :
                 print("le croupier s'arrete")
                 victoire = "moi"
-                return victoire,val_croupier
+                del(c)
+                del(main_croupier)
+                del(paquet_croupier)
+                return victoire,val_croupier,paquet
             elif val_croupier == 21 :
                 print("le croupier s'arrete")
                 victoire = "lui"
-                return victoire,val_croupier
+                del(c)
+                del(main_croupier)
+                del(paquet_croupier)
+                return victoire,val_croupier,paquet
             elif val_croupier == val_joueur :
                 print("le croupier s'arrete")
                 victoire = "lui"
-                return victoire,val_croupier
+                del(c)
+                del(main_croupier)
+                del(paquet_croupier)
+                return victoire,val_croupier,paquet
             elif val_croupier > val_joueur and val_croupier < 21 :
                 print("le croupier s'arrete")
                 victoire = "lui"
-                return victoire,val_croupier
+                del(c)
+                del(main_croupier)
+                del(paquet_croupier)
+                return victoire,val_croupier,paquet
 
             
 def resultat(victoire,val_croupier,val_joueur,solde,mise):
     print("pour rappel, vous avez un total de :",val_joueur,"le croupier quand a lui a :",val_croupier)
     if victoire == "lui":
         print("victoire du croupier")
-        solde = solde - mise
-        print("votre solde estd de :",solde)
+        solde = solde
+        sleep(2)
+        return solde
     else :
         print("vous avez gagnez")
-        print("votre solde estd de :",solde)
- 
+        solde = solde + mise*2
+        sleep(2)
+        return solde
+    
+def parid(pari,pari2,pair,solde):
+    if pair != 0:
+        if pari == pari2:
+            solde = solde + pair
+            print("votre solde est de :",solde)
+            return solde
+        elif pari != pari2:
+            solde = solde - pair
+            print("votre solde est de :",solde)
+            return solde
+    else:
+        solde = solde
+        print("votre solde est de :",solde)
+        return solde
+
